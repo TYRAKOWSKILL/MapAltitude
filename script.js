e2cbd23778aac19ca323f27ceba9d46d6dcc08a2
Erwan CHESNAY <erwan.chesnay@outlook.fr>
2020-02-06 15:56:46 +0100

    Final commit

--- script.js
+++ script.js
@@ -6,10 +6,28 @@ window.onload = function() {
     document.getElementById('buttonOk').addEventListener('click', () => {
         var ville1 = document.getElementById('ville1').value;
         var ville2 = document.getElementById('ville2').value;
-        this.myDirection(ville1, ville2);
-        this.getElevation(ville1, ville2);
-
+        if (ville1.toLowerCase() != ville2.toLowerCase()) {
+            this.myDirection(ville1, ville2);
+            this.getElevation(ville1, ville2);
+        } else {
+            alert('Veuillez entrer des adresses différentes');
+        }
     }, true);
+    document.getElementById('buttonClean').addEventListener('click', () => {
+        map.remove();
+        this.initMap();
+        document.getElementById('chart').setAttribute('src', "");
+    }, true);
+    var placesAutocomplete1 = places({
+        appId: 'pl15CLHNX7LN',
+        apiKey: 'c7637a953638fb67641497336c9ba7e8',
+        container: document.querySelector('#ville1')
+    });
+    var placesAutocomplete2 = places({
+        appId: 'pl15CLHNX7LN',
+        apiKey: 'c7637a953638fb67641497336c9ba7e8',
+        container: document.querySelector('#ville2')
+    });
 }
 
 function initMap() {
@@ -54,14 +72,25 @@ function getElevation(ville1, ville2) {
 }
 
 function setElevation(stringGeoLoc) {
-    var url= 'http://open.mapquestapi.com/elevation/v1/chart?key=YCkvK78NdGpW7eQx5LwDX5inOZHxUUsb&shapeFormat=raw&width=800&height=800&latLngCollection='+ stringGeoLoc;
-    fetch(url)
-        .then(res=>{return res.blob()})
-        .then(blob=>{
-            var img = URL.createObjectURL(blob);
-            // Do whatever with the img
-            document.getElementById('chart').setAttribute('src', img);
-        })
-    
+    var url= 'http://open.mapquestapi.com/elevation/v1/chart?key=YCkvK78NdGpW7eQx5LwDX5inOZHxUUsb&shapeFormat=raw&width=800&height=400&latLngCollection='+ stringGeoLoc;
+    var request = new XMLHttpRequest();
+    request.open('GET', url);
+    request.responseType = 'json';
+    request.send();
 
+    request.onload = function() {
+        var error = request.getResponseHeader("MQ-Error-Message");
+        if (error) {
+            document.getElementById('chart').setAttribute('src', "");
+            alert("La distance est trop grande pour le calcul de l'élévation");
+        } else {
+            fetch(url)
+                .then(res=>{return res.blob()})
+                .then(blob=>{
+                    var img = URL.createObjectURL(blob);
+                    // Do whatever with the img
+                    document.getElementById('chart').setAttribute('src', img);
+                })
+        }
+    }
 }
